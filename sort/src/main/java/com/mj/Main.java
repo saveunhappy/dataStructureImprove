a0dcae63c4b4e648874000406c5ad34e04acb1fc
JiangTaoHou <925147954@qq.com>
2021-12-01 17:40:38 +0800

    冒泡排序增加注释

--- sort/src/main/java/com/mj/Main.java
+++ sort/src/main/java/com/mj/Main.java
@@ -3,7 +3,9 @@ package com.mj;
 public class Main {
     public static void main(String[] args) {
         int[] array = {10,9,19,28,37,56,34};
+        //第一层循环就是最右边一直往左边走，因为你每一次都能选出最大的，在最右边，每次做减减的操作
         for (int end = array.length - 1; end > 0; end--) {
+            //从一开始，所以下面写的就是和begin - 1开始了，和前面的进行比较，就是从0开始了
             for (int begin = 1; begin <= end; begin++) {
                 if(array[begin] < array[begin - 1]){
                     int temp = array[begin];
